bc99cbb0c6a52d754280357380a0af6b54659432
termuze <wertman99999@gmail.com>
2025-07-28 16:33:09 +0300

    fix issue in auth

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.job_list_view, name="job_list"),
+    path("jobs", views.job_list_view, name="job_list"),
     path("jobs/<int:pk>/", views.job_detail_view, name="job_detail"),
 ]
