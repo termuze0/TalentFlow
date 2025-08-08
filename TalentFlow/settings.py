8e8bfa47cd6cb172fa591b7684046e8e07a7a621
termuze <wertman99999@gmail.com>
2025-08-08 21:38:02 +0300

    change host

--- TalentFlow/settings.py
+++ TalentFlow/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv)
+ALLOWED_HOSTS = ["talentflow-udz7.onrender.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
